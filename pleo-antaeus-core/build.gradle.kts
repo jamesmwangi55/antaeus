c3a9e547e8c9fc29a29352f8e25d6862fda6affc
jamesmwangi <james.mwangi@mastercard.com>
2019-07-22 12:04:19 +0300

    Create charge invoice function

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 kotlinProject()
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
     implementation(project(":pleo-antaeus-data"))
     compile(project(":pleo-antaeus-models"))
 }
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -2,13 +2,37 @@ package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.data.AntaeusDal
+import kotlinx.coroutines.*
 
+@ObsoleteCoroutinesApi
 class BillingService(
     private val paymentProvider: PaymentProvider,
     private val dal: AntaeusDal
 ) {
-    fun charge() {
-        val invoices = dal.fetchPendingInvoices()
+
+    val handler = CoroutineExceptionHandler{coroutineContext, throwable ->
 
     }
+
+    suspend fun charge() = coroutineScope{
+        val invoices = dal.fetchPendingInvoices()
+
+
+        // Since payment provider is an external service
+        // run it in a coroutine to avoid blocking the main thread.
+        // This code also runs asynchronously hence faster.
+        val deferredResults = invoices.map { invoice ->
+            async (handler) {
+                val result = paymentProvider.charge(invoice)
+                Pair(invoice, result)
+            }
+        }
+
+        // get invoices that were successfully charged
+        val results = deferredResults.awaitAll().filter { it.second }.map { it.first }
+
+
+    }
+
+
 }
