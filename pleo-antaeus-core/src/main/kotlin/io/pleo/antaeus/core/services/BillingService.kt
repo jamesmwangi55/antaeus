f7e4afa51dc01c9d9de3f6282bffb06bf8981c03
jamesmwangi <james.mwangi@mastercard.com>
2019-07-22 10:53:40 +0300

    Inject AntaeusDal and some code cleanup

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,11 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val dal: AntaeusDal
 ) {
-   // TODO - Add code e.g. here
+
 }
