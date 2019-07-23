8a93d31648a0b5a2bed45668e34045c2ce6b4f7e
jamesmwangi <james.mwangi@mastercard.com>
2019-07-23 16:25:34 +0300

    Stop using global scope for job

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/BillingServiceJob.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/BillingServiceJob.kt
@@ -1,18 +1,24 @@
 package io.pleo.antaeus.app
 
 import io.pleo.antaeus.core.services.BillingService
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.quartz.SchedulerContext
 import org.quartz.SchedulerException
+import kotlin.coroutines.CoroutineContext
 
-class BillingServiceJob: Job {
+class BillingServiceJob : Job, CoroutineScope {
+
+    private val job: kotlinx.coroutines.Job = kotlinx.coroutines.Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     override fun execute(context: JobExecutionContext?) {
-        GlobalScope.launch (Dispatchers.Default) {
+        launch (Dispatchers.Default) {
             var schedulerContext: SchedulerContext? = null
 
             try {
